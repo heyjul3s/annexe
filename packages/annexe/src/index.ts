fa59eb0f12cbab597c214d327afaac6eebde7f8f
heyjul3s <julian.cy.low@gmail.com>
2021-03-04 15:58:32 +1100

    update main annexe dependencies and exports

--- packages/annexe/src/index.ts
+++ packages/annexe/src/index.ts
@@ -1,3 +1,4 @@
+export { isArray } from '@annexe/isarray';
 export { isFloat } from '@annexe/isfloat';
 export { isNumber } from '@annexe/isnumber';
 export { isString } from '@annexe/isstring';
@@ -6,3 +7,8 @@ export { isFinite } from '@annexe/isfinite';
 export { isNonEmptyString } from '@annexe/isnonemptystring';
 export { isPlainObject } from '@annexe/isplainobject';
 export { isNegative } from '@annexe/isnegative';
+export { partial } from '@annexe/partial';
+export { partialRight } from '@annexe/partialright';
+export { reverse } from '@annexe/reverse';
+export { head } from '@annexe/head';
+export { tail } from '@annexe/tail';
