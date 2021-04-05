e41b535641d4e7c4fbe26b5692712b67c7f7c5b7
heyjul3s <julian.cy.low@gmail.com>
2021-04-05 11:07:04 +1000

    create types package

---
+++ packages/types/src/index.ts
@@ -0,0 +1,33 @@
+import { isArray } from '@annexe/isarray';
+import { isFinite } from '@annexe/isfinite';
+import { isFloat } from '@annexe/isfloat';
+import { isMap } from '@annexe/ismap';
+import { isNegative } from '@annexe/isnegative';
+import { isNil } from '@annexe/isnil';
+import { isNonEmptyString } from '@annexe/isnonemptystring';
+import { isNumber } from '@annexe/isnumber';
+import { isPlainObject } from '@annexe/isplainobject';
+import { isSet } from '@annexe/isset';
+import { isString } from '@annexe/isstring';
+import { isTypedArray } from '@annexe/istypedarray';
+import { isWeakMap } from '@annexe/isweakmap';
+import { isWeakSet } from '@annexe/isWeakSet';
+
+const types = {
+  isArray,
+  isFinite,
+  isFloat,
+  isMap,
+  isNegative,
+  isNil,
+  isNonEmptyString,
+  isNumber,
+  isPlainObject,
+  isSet,
+  isString,
+  isTypedArray,
+  isWeakMap,
+  isWeakSet
+};
+
+export default types;
